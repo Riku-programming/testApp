bed98bedad105936fdb4be0743e97d2d1bb17885
Riku <joker_avan@icloud.com>
2020-03-06 13:02:29 +0900

    Initial Commit

---
+++ testApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Riku on 2020/03/06.
+//  Copyright © 2020 Riku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
